9ab7123185119797475383bd92e5b768ed225573
Reza Piri <charlieecho2017@gmail.com>
2018-09-04 15:14:47 -0500

    gitignore

---
+++ config.js
@@ -0,0 +1,21 @@
+const config = {};
+
+config.callbackRoot = "[https:/#{your-app-name}.herokuapp.com/]";
+
+config.opUri = "https://srv.qryp.to";
+
+config.qryptoAuth = {
+  clientID: "[OPENID_CLIENT_ID]",
+  clientSecret: "[OPENID_CLIENT_SECRET]",
+  authorizationURL: `${config.opUri}/op/auth`,
+  tokenURL: `${config.opUri}/op/token`,
+  userInfoURL: `${config.opUri}/op/me`,
+  callbackURL: `${config.callbackRoot}/auth/qrypto/callback`,
+  skipUserProfile: false,
+  issuer: `${config.opUri}/op`,
+  passReqToCallback: true
+};
+
+config.mongoUrl = "[YOUR MongoDB connection URL]";
+
+module.exports = config;
