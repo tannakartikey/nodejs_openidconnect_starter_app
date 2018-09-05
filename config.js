3b4897bd52adcced1293bec2da51141b4ffb71b3
Reza Piri <charlieecho2017@gmail.com>
2018-09-04 22:30:39 -0500

    brackets in encoding

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ config.callbackRoot = "[https:/da-nodejs.herokuapp.com]";
 config.opUri = "https://srv.qryp.to";
 
 config.qryptoAuth = {
-  clientID: "[0474a72a-3a58-482c-89bb-e1f6bcd4e7cb]",
-  clientSecret: "[5J3m572iiRDAnnZjLT4uRLWS4vyjeEW3k1YHu9K+0VZmIq5on21bAabJVKZkof9t]",
+  clientID: "0474a72a-3a58-482c-89bb-e1f6bcd4e7cb",
+  clientSecret: "5J3m572iiRDAnnZjLT4uRLWS4vyjeEW3k1YHu9K+0VZmIq5on21bAabJVKZkof9t",
   authorizationURL: `${config.opUri}/op/auth`,
   tokenURL: `${config.opUri}/op/token`,
   userInfoURL: `${config.opUri}/op/me`,
